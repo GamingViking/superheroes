034139c306dbd143f9d5e7bc875a7b33b6a4a691
Chris Schmidt <cmschmidt12345@gmail.com>
2024-08-28 13:31:49 -0400

    adjusted width of names next to powerstats to be responsive when no powerstat data is found

--- app/components/Powerstat.tsx
+++ app/components/Powerstat.tsx
@@ -10,11 +10,11 @@ type PowerstatProps = {
 const Powerstat: React.FC<PowerstatProps> = ({ powerstat, statname, icon }) => {
     return (
         <div className="mt-1 relative flex flex-row">                 
-            <div className="w-[22%] relative flex flex-row">
+            <div className="w-[25%] relative flex flex-row">
                 <div>{icon}</div>
                 <div className="mt-1 mr-1">: {powerstat}</div>
             </div>
-            <div className="w-full h-8 flex flex-row justify-end bg-gradient-to-r from-yellow-200 via-orange-400 to-red-500">
+            <div className="w-full flex-shrink h-8 flex flex-row justify-end bg-gradient-to-r from-yellow-200 via-orange-400 to-red-500">
                 <div className="z-10 absolute p-1">{statname}</div>
                 <div className={`relative h-8  bg-slate-400`} style={{width: `${100 - powerstat}%`}}></div>
             </div>
