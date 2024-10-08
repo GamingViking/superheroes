7f89b7312b93b8c6f7a2520439bce3891e1aba09
Chris Schmidt <cmschmidt12345@gmail.com>
2024-10-08 18:21:45 -0400

    made LCP slightly smaller to account for CCLS alterations

--- app/components/HeroInfoContainer.tsx
+++ app/components/HeroInfoContainer.tsx
@@ -14,7 +14,7 @@ const HeroInfoContainer: React.FC<HeroInfoContainerProps> = ({ heroInfo, aliases
         <div className="flex flex-col items-center md:items-start md:justify-center md:flex-row m-3 bg-fuchsia-950 border-2 border-zinc-950 rounded-lg md:h-96">
             {heroInfo?.image?.url && 
                 <div className="justify-center mx-1">
-                <img src={heroInfo?.image?.url} alt={`${heroInfo.name} enlarged image`} className="object-cover w-72 h-96" fetchPriority="high"/>
+                <img src={heroInfo?.image?.url} alt={`${heroInfo.name} enlarged image`} className="object-cover w-72 h-[380px]" fetchPriority="high"/>
                 </div>} 
             <div className="p-2 mx-1 xs:w-96 w-80 text-white">
                 <div className="flex flex-row">
